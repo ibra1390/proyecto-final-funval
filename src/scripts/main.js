6b63b10b74e25cfa3cbf61c1c354879ff196abb5
ibraim <ing.gonzalezvij@hotmail.com>
2025-10-28 14:47:18 -0600

    filtro de ubicaciones casi funcionando

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,12 @@
 import { stays } from "./stays.js";
 import { renderStays } from "./utils.js";
 import { initModal } from "./modal.js"
+import { initFilters } from "./filters.js"
 
 function init() {
     renderStays(stays);
     initModal();
+    initFilters();
 }
 
 document.addEventListener("DOMContentLoaded", init);
